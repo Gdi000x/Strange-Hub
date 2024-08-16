c9fa7f77b61fac348011fe91f7355e9b0dfe251d
Gdi000x <151930238+Gdi000x@users.noreply.github.com>
2024-08-16 20:02:01 +0700

    Create StrangeHub.lua

---
+++ StrangeHub.lua
@@ -0,0 +1,173 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Strange Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "StrangeHu"})
+local Tab = Window:MakeTab({
+	Name = "Scripts",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+local Section = Tab:AddSection({
+	Name = "All Scripts"
+})
+Tab:AddButton({
+	Name = "FE FLING TOOL",
+	Callback = function()
+      		loadstring(game:HttpGet("https://pastebin.com/raw/3BBfwsmL"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Fly Gui",
+	Callback = function()
+      		loadstring(game:HttpGet("https://pastebin.com/raw/wP6nag0K")()
+  	end    
+})
+Tab:AddButton({
+	Name = "Brookhaven Script", 
+	Callback = function()
+      		loadstring(game:HttpGet("https://pastebin.com/raw/3BBfwsmL"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Blox Fruits - W-azure Hub",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/BasementHub/BH-Blox-fruit/main/W-Azure-V3"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "MM2 - Kidachi Hub",
+	Callback = function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KidichiHB/Kidachi/main/Scripts/MM2", true))() 
+  	end    
+})
+Tab:AddButton({
+	Name = "Arsenal - Cat Hub",
+	Callback = function()
+loadstring(game:HttpGet('https://whimper.xyz/cathub.lua'))() 
+  	end    
+})
+Tab:AddButton({
+	Name = "Taxi Boss Script",
+	Callback = function()
+	loadstring(game:HttpGet("https://scriptblox.com/raw/Taxi-Boss-GROUPS-OP-Auto-Farm-Script-9442"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Aimbot x WallHack",
+	Callback = function()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Mick-gordon/Hyper-Escape/main/DeleteMobV2.lua", true))() 
+  	end    
+})
+Tab:AddButton({
+	Name = "Aimbot x WallHack",
+	Callback = function()
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Mick-gordon/Hyper-Escape/main/DeleteMobV2.lua", true))() 
+  	end    
+})
+Tab:AddButton({
+	Name = "Da Hood - CGHub",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/botdevXD/CGHub/main/Loader.lua", true))() 
+  	end    
+})
+Tab:AddButton({
+	Name = "Blade Ball Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/kiurgjirkgk/NightForRoblox/main/loader.lua"))() 
+  	end    
+})
+Tab:AddButton({
+	Name = "TOH - Garfield Hub",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/2dgeneralspam1/scripts-and-stuff/master/scripts/garfield%20hub", true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Murders VS Sheriffs Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://github.com/OfficiallyMelon/melons_script_hub/raw/main/Protected_1436831316964821.txt"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "GYM League Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Gym%20League.lua"))() 
+  	end    
+})
+Tab:AddButton({
+	Name = "Blade Ball Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/kiurgjirkgk/NightForRoblox/main/loader.lua"))() 
+  	end    
+})
+Tab:AddButton({
+	Name = "Prison Life Script",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/elliexmln/PrizzLife/main/pladmin.lua'))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Fling Things and People Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/BlizTBr/scripts/main/FTAP.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Trident Survival Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://efial.wtf/Scripts/Trident%20(NEW).lua",true))() 
+  	end    
+})
+Tab:AddButton({
+	Name = "Ragdoll Engine Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/H20CalibreYT/SystemBroken/main/script"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Project Slayers Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/cloudman4416/scripts/main/Loader"))()
+  	end    
+}) 
+Tab:AddButton({
+	Name = "Infinite Yield",
+	Callback = function()
+      		loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))() 
+  	end    
+})
+Tab:AddButton({
+	Name = "Blade Ball - FFJ HUB",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/FFJ1/Roblox-Exploits/main/scripts/BladeBallV3.lua"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Car Dealer Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/IExpIoit/Script/main/UltimateHub"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "a dusty trip Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/artemy133563/Utilities/main/ADustyTrip",true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Jujutsu Shenanigans Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/LOLking123456/Jujutsu/main/Shenanigans"))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Doors Script",
+	Callback = function()
+      		loadstring(game:HttpGet("https://raw.githubusercontent.com/sinret/rbxscript.com-scripts-reuploads-/main/dars", true))()
+  	end    
+})
+Tab:AddButton({
+	Name = "Jailbreak Script",
+	Callback = function()
+      		loadstring(game:HttpGet('https://api.luarmor.net/files/v3/loaders/730854e5b6499ee91deb1080e8e12ae3.lua'))()
+  	end    
+})
+OrionLib:Init()
